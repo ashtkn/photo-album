8b6855ab5be8c174569fb7e76bcf5c3729ce96da
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-10-28 17:33:25 +0900

    fix: API function

--- pages/api/photo-list.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-import photos from '../../constants/photos'
-
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-  res.status(200).json(photos)
-}
---
+++ pages/api/photos.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import photos from '../../constants/photos'
+
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+  res.status(200).json({
+    data: photos.map(({ title, filename }) => {
+      const baseUrl = req.headers.host
+      return {
+        title,
+        image: `${baseUrl}/images/${filename}`,
+      }
+    }),
+  })
+}
